77a7fc6e9cf0e6785da16e342ea5ed6fcb3883dc
Antonin HUAUT <21803199@etu.unicaen.fr>
2019-09-27 15:38:25 +0200

    up to 19h

--- apps/static/js/consulEdt/showEDT.js
+++ apps/static/js/consulEdt/showEDT.js
@@ -28,7 +28,7 @@ function loadCalendar(listEvents, options) {
         allDaySlot: false,
         allDayText: false,
         minTime: "08:00:00",
-        maxTime: "18:30:00",
+        maxTime: "19:00:00",
         nowIndicator: true,
         //
         // Methods
